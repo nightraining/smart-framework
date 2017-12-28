04355dc29459b6fc061e3709b52b1e65f030220b
nightraining <lizhenghui@thit.com.cn>
2017-12-28 15:23:51 +0800

    类扫描器

---
+++ src/main/java/org/smart4j/framework/core/ClassScanner.java
@@ -0,0 +1,34 @@
+package org.smart4j.framework.core;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+/**
+ * 类扫描器
+ */
+public interface ClassScanner {
+
+    /**
+     * 获取指定包名下的所有类
+     * @param packageName
+     * @return
+     */
+    List<Class<?>> getClass(String packageName);
+
+    /**
+     * 获取指定包名中所指定注解的相关类
+     * @param packageName
+     * @param annotationClass
+     * @return
+     */
+    List<Class<?>> getClassListByAnnotation(String packageName, Class<? extends Annotation> annotationClass);
+
+
+    /**
+     * 获取指定包名中指定父类或接口的相关类
+     * @param packageName
+     * @param superClass
+     * @return
+     */
+    List<Class<?>> getClassListBySuper(String packageName, Class<?> superClass);
+}
