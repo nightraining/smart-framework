f439d77cd7c4b5d127b7be80b28b6c3e6f35ea32
nightraining <lizhenghui@thit.com.cn>
2017-12-28 15:12:45 +0800

    用于获取子类的模板类

---
+++ src/main/java/org/smart4j/framework/core/impl/support/SupperClassTemplate.java
@@ -0,0 +1,14 @@
+package org.smart4j.framework.core.impl.support;
+
+/**
+ * 用于获取子类的模板类
+ */
+public abstract class SupperClassTemplate extends ClassTemplate{
+
+    protected final Class<?> superClass;
+
+    protected SupperClassTemplate(String packageName, Class<?> superClass){
+        super(packageName);
+        this.superClass = superClass;
+    }
+}
