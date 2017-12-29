b4d412f84cdeb34b54ad9dcc95fd3cf1d800ab73
nightraining <lizhenghui@thit.com.cn>
2017-12-29 15:35:43 +0800

    封装 Request 对象相关信息

---
+++ src/main/java/org/smart4j/framework/mvc/Requester.java
@@ -0,0 +1,23 @@
+package org.smart4j.framework.mvc;
+
+/**
+ * 封装 Request 对象相关信息
+ */
+public class Requester {
+
+    private String requestMethod;
+    private String requestPath;
+
+    public Requester(String requestMethod, String requestPath) {
+        this.requestMethod = requestMethod;
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+}
