e239d83a89f949697824c939f00ca1fee14e5cf8
nightraining <lizhenghui@thit.com.cn>
2018-01-02 11:27:29 +0800

    上传文件异常

---
+++ src/main/java/org/smart4j/framework/mvc/dault/UploadException.java
@@ -0,0 +1,22 @@
+package org.smart4j.framework.mvc.dault;
+
+/**
+ * 上传异常（当文件上传失败时抛出）
+ */
+public class UploadException extends RuntimeException{
+
+    public UploadException() {
+    }
+
+    public UploadException(String message) {
+        super(message);
+    }
+
+    public UploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UploadException(Throwable cause) {
+        super(cause);
+    }
+}
