58c4f3744a88ff668d0bdede336e19d47c61d6a9
nightraining <lizhenghui@thit.com.cn>
2018-01-03 11:34:07 +0800

    代理接口添加注释

--- src/main/java/org/smart4j/framework/aop/proxy/Proxy.java
+++ src/main/java/org/smart4j/framework/aop/proxy/Proxy.java
@@ -7,9 +7,9 @@ public interface Proxy {
 
     /**
      * 执行链式代理
-     * @param proxyChain
-     * @return
-     * @throws Throwable
+     * @param proxyChain 代理链
+     * @return 目标方代理返回值
+     * @throws Throwable 异常
      */
     Object doProxy(ProxyChain proxyChain) throws Throwable;
 }
